3e7a854808917dc32bccd98280c937c517896b4c
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-02-10 16:59:39 +0530

    Added menus usage
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,11 @@ import {
 
 export default function DefaultPage() {
   const [checkBoxValue, setCheckBoxValue] = useState<boolean | null>(false);
+  const [inputField, setInputField] = useState<string>('');
+
+  const handleInputFieldChange = (value: string) => {
+    setInputField(value);
+  }
   return (
     <Scaffold
       bottomAppBar={
@@ -70,7 +75,7 @@ export default function DefaultPage() {
                 </TextButton>
               </div>
             </div>
-            <div className="p-4 border border-dashed w-max flex flex-col gap-3 mx-2 my-5">
+            <div className="bg-blue-200 p-4 border border-dashed w-max flex flex-col gap-3 mx-2 my-5">
               <div className="mb-4  text-[rgb(var(--md-sys-color-on-background))]">
                 <h3 className="text-xl font-medium">TextFields</h3>
                 <p className="max-w-[40ch]">
@@ -78,14 +83,16 @@ export default function DefaultPage() {
                 </p>
               </div>
               <div className="grid grid-cols-2 gap-4">
-                <FilledTextField labelText="Hello" value={""} onValueChange={function (value: string): void {
-                  throw new Error("Function not implemented.");
-                }} />
+                <FilledTextField labelText="Hello" value={inputField} onValueChange={(e) => handleInputFieldChange(e)} />
+                <FilledTextField type="dropdown" options={['US', 'EN', 'IN']} labelText="Dropdown" value={inputField} onValueChange={(e) => handleInputFieldChange(e)} />
                 <OutlinedTextField
                   labelText="Outlined"
-                  leadingIcon="account_circle" value={""} onValueChange={function (value: string): void {
-                    throw new Error("Function not implemented.");
-                  }} />
+                  leadingIcon="account_circle" value={inputField} onValueChange={(e) => handleInputFieldChange(e)} />
+                <OutlinedTextField
+                  type="dropdown"
+                  options={['US', 'EN', 'IN']}
+                  labelText="Outlined"
+                  leadingIcon="account_circle" value={inputField} onValueChange={(e) => handleInputFieldChange(e)} />
               </div>
             </div>
             <div className="p-4 border border-dashed w-max flex flex-col gap-3 mx-2 my-5">
@@ -217,83 +224,136 @@ export default function DefaultPage() {
             </div>
 
           </div>
-          <div className="flex justify-center items-center mx-auto mb-20">
-              <Menu>
-                <MenuItem
-                  leadingIcon={"delete"}
-                  label={"Delete"}
-                  trailingText={"del"}
-                />
-                <MenuItem
-                  leadingIcon={"logout"}
-                  trailingIcon={"check"}
-                  label={"Logout"}
-                  trailingText={""}
-                  disable
-                />
-                <MenuItem
-                  leadingIcon={"note_stack"}
-                  trailingIcon={"More Options"}
-                  label={"Child"}
-                  trailingText={""}
-                >
-                  <Menu>
-                    <MenuItem
-                      leadingIcon={"grade"}
-                      label={"Grade"}
-                      trailingText={""}
-                    />
-                    <MenuItem
-                      leadingIcon={"apps"}
-                      label={"Apps"}
-                      trailingText={""}
-                    >
-                      <Menu>
-                        <MenuItem
+          <div className="flex flex-col justify-center items-center mx-auto mb-20">
+            <h3 className="text-xl font-medium text-white">Menus</h3>
+            <Menu>
+              <MenuItem
+                leadingIcon={"delete"}
+                label={"Delete"}
+                trailingText={"del"}
+              />
+              <MenuItem
+                leadingIcon={"logout"}
+                trailingIcon={"check"}
+                label={"Logout"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"note_stack"}
+                trailingIcon={"More Options"}
+                label={"Child"}
+                trailingText={""}
+              >
+                <Menu>
+                  <MenuItem
+                    leadingIcon={"grade"}
+                    label={"Grade"}
+                    trailingText={""}
+                  />
+                  <MenuItem
+                    leadingIcon={"apps"}
+                    trailingIcon={""}
+                    label={"Apps"}
+                    trailingText={""}
+                  >
+                    <Menu>
+                      <MenuItem
                         leadingIcon={"bolt"}
                         label={"Bolt"}
                         trailingText={"ctlr+A"}
-                        />
-                      </Menu>
-                    </MenuItem>
-                    <MenuItem
-                      leadingIcon={"key"}
-                      label={"Key"}
-                      trailingText={""}
-                    />
-                  </Menu>
-                </MenuItem>
-                <MenuItem
-                  leadingIcon={"open_run"}
-                  label={"Open Run"}
-                  trailingText={""}
-                />
-                <Divider type="horizontal" variant="fullWidth" />
-                <MenuItem
-                  leadingIcon={"open_run"}
-                  label={"Open Run"}
-                  trailingText={"cltr+R"}
-                />
-                <MenuItem
-                  leadingIcon={"table"}
-                  label={"Table"}
-                  trailingText={""}
-                  disable
-                />
-                <MenuItem
-                  leadingIcon={"image"}
-                  label={"Image"}
-                  trailingText={""}
-                  disable
-                />
-                <MenuItem
-                  leadingIcon={"dashboard"}
-                  label={"Dashboard"}
-                  trailingText={""}
-                  disable
-                />
-              </Menu>
-            </div>
+                      />
+                    </Menu>
+                  </MenuItem>
+                  <MenuItem
+                    leadingIcon={"key"}
+                    label={"Key"}
+                    trailingText={""}
+                  />
+                </Menu>
+              </MenuItem>
+              <MenuItem
+                leadingIcon={"open_run"}
+                label={"Open Run"}
+                trailingText={""}
+              />
+              <Divider type="horizontal" variant="fullWidth" />
+              <MenuItem
+                leadingIcon={"open_run"}
+                label={"Open Run"}
+                trailingText={"cltr+R"}
+              />
+              <MenuItem
+                leadingIcon={"table"}
+                label={"Table"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"image"}
+                label={"Image"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"dashboard"}
+                label={"Dashboard"}
+                trailingText={""}
+                disable
+              />
+            </Menu>
+          </div>
+          <div className="flex flex-col justify-center items-center mx-auto mb-20">
+            <h3 className="text-xl font-medium text-white">Menus-scrollable</h3>              
+            <Menu displayLimit={5}>
+              <MenuItem
+                leadingIcon={"delete"}
+                label={"Delete"}
+                trailingText={"del"}
+              />
+              <MenuItem
+                leadingIcon={"logout"}
+                trailingIcon={"check"}
+                label={"Logout"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"note_stack"}
+                label={"Child"}
+                trailingText={""}
+              />
+              <MenuItem
+                leadingIcon={"open_run"}
+                label={"Open Run"}
+                trailingText={""}
+              />
+              <Divider type="horizontal" variant="fullWidth" />
+              <MenuItem
+                leadingIcon={"open_run"}
+                label={"Open Run"}
+                trailingText={"cltr+R"}
+              />
+              <MenuItem
+                leadingIcon={"table"}
+                label={"Table"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"image"}
+                label={"Image"}
+                trailingText={""}
+                disable
+              />
+              <MenuItem
+                leadingIcon={"dashboard"}
+                label={"Dashboard"}
+                trailingText={""}
+                disable
+              />
+            </Menu>
+          </div>
           <div className="h-16" />
         </div>
       )}
